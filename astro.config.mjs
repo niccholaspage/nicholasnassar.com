f4c02f35990560fc517cca46a2055adcc957dbb2
Nicholas Nassar <niccholaspage@gmail.com>
2022-10-29 13:27:19 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import Unocss from 'unocss/astro';
+
+import { defineConfig } from 'astro/config';
+import { presetUno } from 'unocss';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        Unocss({
+            presets: [
+                presetUno()
+            ]
+        })
+    ]
+});
