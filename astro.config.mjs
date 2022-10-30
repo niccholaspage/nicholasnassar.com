f3a616c22889970f5c1211edc999f97300510113
Nicholas Nassar <niccholaspage@gmail.com>
2022-10-29 23:30:19 -0400

    Switch to Tailwind to fix some issues with UnoCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
-import Unocss from "unocss/astro";
-
 import { defineConfig } from "astro/config";
-import { presetUno } from "unocss";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    Unocss({
-      presets: [presetUno({
-        dark: "media"
-      })],
-    }),
-  ],
+  integrations: [tailwind()]
 });
