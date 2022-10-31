e818af119800345028914bf4acd695d6e510388b
Nicholas Nassar <niccholaspage@gmail.com>
2022-10-31 16:34:53 -0400

    Close mobile menu when clicking on a link in the header

--- src/components/nav/mobile/MobileMenu.tsx
+++ src/components/nav/mobile/MobileMenu.tsx
@@ -1,14 +1,30 @@
-import type { Component } from "solid-js";
-import { isMobileMenuOpen } from "./MobileMenuState";
+import { Component, onCleanup, onMount } from "solid-js";
+import { isMobileMenuOpen, setMobileMenuOpen } from "./MobileMenuState";
 
 interface MobileMenuProps {
   children: any;
 }
 
 export const MobileMenu: Component<MobileMenuProps> = (props) => {
+  let menuDiv: HTMLDivElement | undefined;
+
+  // We need to close the menu when the user
+  // clicks on one of the items in the menu.
+  const handleItemClick = () => {
+    setMobileMenuOpen(false);
+  };
+
+  onMount(() => {
+    menuDiv?.addEventListener("click", handleItemClick);
+  });
+
+  onCleanup(() => {
+    menuDiv?.removeEventListener("click", handleItemClick);
+  });
+
   const classes = () => (isMobileMenuOpen() ? "sm:hidden" : "hidden");
   return (
-    <div class={classes()} id="mobile-menu">
+    <div ref={menuDiv} class={classes()} id="mobile-menu">
       {props.children}
     </div>
   );
