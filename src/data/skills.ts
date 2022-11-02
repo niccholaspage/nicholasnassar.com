cb9b804220e7b9ecdc89e41f281270c9ab59bb13
Nicholas Nassar <niccholaspage@gmail.com>
2022-11-02 13:52:15 -0400

    Initial version of skills section of homepage

---
+++ src/data/skills.ts
@@ -0,0 +1,179 @@
+interface Skill {
+  name: string;
+  image: string;
+  link: string;
+}
+
+const skills: Skill[] = [
+  {
+    name: "Apache",
+    image: "apache",
+    link: "https://httpd.apache.org/",
+  },
+  {
+    name: "AWS",
+    image: "aws",
+    link: "https://aws.amazon.com/",
+  },
+  {
+    name: "Bash",
+    image: "bash",
+    link: "https://en.wikipedia.org/wiki/Bash_(Unix_shell)",
+  },
+  {
+    name: "Bootstrap",
+    image: "bootstrap",
+    link: "https://getbootstrap.com/",
+  },
+  {
+    name: "C++",
+    image: "cpp",
+    link: "https://en.wikipedia.org/wiki/C%2B%2B",
+  },
+  {
+    name: "C#",
+    image: "csharp",
+    link: "https://learn.microsoft.com/en-us/dotnet/csharp/",
+  },
+  {
+    name: "CSS",
+    image: "css",
+    link: "https://en.wikipedia.org/wiki/CSS",
+  },
+  {
+    name: "Dart",
+    image: "dart",
+    link: "https://dart.dev/",
+  },
+  {
+    name: "Deno",
+    image: "denojs",
+    link: "https://deno.land/",
+  },
+  {
+    name: "DigitalOcean",
+    image: "digitalocean",
+    link: "https://www.digitalocean.com/",
+  },
+  {
+    name: "Docker",
+    image: "docker",
+    link: "https://www.docker.com/",
+  },
+  {
+    name: "Flutter",
+    image: "flutter",
+    link: "https://flutter.dev/",
+  },
+  {
+    name: "Git",
+    image: "git",
+    link: "https://git-scm.com/",
+  },
+  {
+    name: "Gradle",
+    image: "gradle",
+    link: "https://gradle.org/",
+  },
+  {
+    name: "HTML5",
+    image: "html5",
+    link: "https://en.wikipedia.org/wiki/HTML5",
+  },
+  {
+    name: "Hugo",
+    image: "hugo",
+    link: "https://gohugo.io/",
+  },
+  {
+    name: "Java",
+    image: "java",
+    link: "https://www.java.com/",
+  },
+  {
+    name: "JavaScript",
+    image: "javascript",
+    link: "https://en.wikipedia.org/wiki/JavaScript",
+  },
+  {
+    name: "Kubernetes",
+    image: "k8s",
+    link: "https://kubernetes.io/",
+  },
+  {
+    name: "Kotlin",
+    image: "kotlin",
+    link: "https://kotlinlang.org/",
+  },
+  {
+    name: "Linux",
+    image: "linux",
+    link: "https://en.wikipedia.org/wiki/Linux",
+  },
+  {
+    name: "Magento",
+    image: "magento",
+    link: "https://business.adobe.com/products/magento/magento-commerce.html",
+  },
+  {
+    name: "MySQL",
+    image: "mysql",
+    link: "https://www.mysql.com/",
+  },
+  {
+    name: "NodeJS",
+    image: "nodejs",
+    link: "https://nodejs.org/",
+  },
+  {
+    name: "PHP",
+    image: "php",
+    link: "https://www.php.net/",
+  },
+  {
+    name: "PostgreSQL",
+    image: "postgresql",
+    link: "https://www.postgresql.org/",
+  },
+  {
+    name: "Python",
+    image: "python",
+    link: "https://www.python.org/",
+  },
+  {
+    name: "React",
+    image: "react",
+    link: "https://reactjs.org/",
+  },
+  {
+    name: "Rust",
+    image: "rust",
+    link: "https://www.rust-lang.org/",
+  },
+  {
+    name: "SASS",
+    image: "sass",
+    link: "https://sass-lang.com/",
+  },
+  {
+    name: "Tailwind CSS",
+    image: "tailwindcss",
+    link: "https://tailwindcss.com/",
+  },
+  {
+    name: "TypeScript",
+    image: "typescript",
+    link: "https://www.typescriptlang.org/",
+  },
+  {
+    name: "VueJS",
+    image: "vuejs",
+    link: "https://vuejs.org/",
+  },
+  {
+    name: "Webpack",
+    image: "webpack",
+    link: "https://webpack.js.org/",
+  },
+];
+export { skills };
