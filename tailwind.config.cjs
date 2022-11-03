9a9c32a8fb46100d6998b6942d9d7158ca8516b1
Nicholas Nassar <niccholaspage@gmail.com>
2022-11-02 22:16:07 -0400

    Work on typewriter effect for homepage header text

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,32 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "typing-and-blink-caret": "typing 3.5s steps(40, end), blink-caret .75s step-end infinite",
+      },
+      keyframes: {
+        typing: {
+          from: {
+            width: "0",
+          },
+          to: {
+            width: "100%",
+          },
+        },
+        "blink-caret": {
+          "from, to": {
+            "border-color": "transparent",
+          },
+          "50%": {
+            "border-color": "orange",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
