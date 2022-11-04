c117f50e8174ec91c765b71fe905f993d8b7f5f4
Nicholas Nassar <niccholaspage@gmail.com>
2022-11-04 18:31:03 -0400

    Fix mobile menu button styling

--- src/components/nav/mobile/MobileMenuButton.tsx
+++ src/components/nav/mobile/MobileMenuButton.tsx
@@ -14,7 +14,7 @@ export const MobileMenuButton: Component<MobileMenuButtonProps> = (props) => {
     <button
       onClick={toggleMenu}
       type="button"
-      class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+      class="inline-flex items-center justify-center rounded-md p-2 text-violet-400 hover:bg-violet-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
       aria-controls="mobile-menu"
       aria-expanded="false"
     >
