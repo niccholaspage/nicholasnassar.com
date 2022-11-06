f1b0522bb8d9eb8399d28116998c36a4cac25981
Nicholas Nassar <niccholaspage@gmail.com>
2022-11-06 15:45:30 -0500

    Set site variable in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  site: "https://www.nicholasnassar.com",
+  integrations: [tailwind(), solidJs()],
 });
