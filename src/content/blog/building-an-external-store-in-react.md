15b64759b24492ce2042640e9dcbed12fae13404
Nicholas Nassar <niccholaspage@gmail.com>
2023-01-29 13:39:47 -0500

    Initial logic for blog pages and fixes to blog schema and date handling

---
+++ src/content/blog/building-an-external-store-in-react.md
@@ -0,0 +1,6 @@
+---
+title: "Building an External Store in React"
+description: "Ever wondered how libraries like Zustand and Jotai work? Let's find out by building our own external store in React!"
+tags: ["React", "JavaScript", "TypeScript"]
+publishDate: "01-29-2023"
+---
--- src/content/config.ts
+++ src/content/config.ts
@@ -5,8 +5,11 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     tags: z.array(z.string()),
-    publishDate: z.date(),
-    updatedDate: z.date().optional(),
+    publishDate: z.string().transform((str) => new Date(str)),
+    updatedDate: z
+      .string()
+      .transform((str) => new Date(str))
+      .optional(),
   }),
 });
 
