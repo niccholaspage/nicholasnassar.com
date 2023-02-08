93f9a474ed0445ee0fe99df561b1779dbbfa9601
Nicholas Nassar <niccholaspage@gmail.com>
2023-02-08 15:35:27 -0500

    Scan SVGs for Tailwind classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}"],
   theme: {
     extend: {
       animation: {
