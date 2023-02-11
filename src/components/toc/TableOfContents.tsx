116165b3941ea7818f8918bfdbec2195d400a34d
Nicholas Nassar <niccholaspage@gmail.com>
2023-02-10 23:05:07 -0500

    Remove dynamic heading generation that causes hydration issues

--- src/components/toc/TableOfContents.tsx
+++ src/components/toc/TableOfContents.tsx
@@ -1,19 +1,35 @@
 import { ChevronRight } from "../icons/ChevronRight";
 import { RoundedButtonDiv } from "../button/RoundedButton";
 import { createSignal, For, type JSX, type Component } from "solid-js";
-import { getPageHeadings, PageHeading } from "../../util/page-headings";
 
 // TODO: Don't hardcode this
-const pageHeadings = getPageHeadings("/");
-
-const listItems = [...pageHeadings].map((item) => JSON.parse(item)) as PageHeading[];
+const pageHeadings = [
+  {
+    id: "",
+    title: "Intro",
+  },
+  {
+    id: "experience",
+    title: "Experience",
+  },
+  {
+    id: "skills",
+    title: "Skills",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "latest-posts",
+    title: "Latest Posts",
+  },
+];
 
 export const TableOfContents: Component = () => {
   const [open, setOpen] = createSignal(false);
   const onToggle: JSX.EventHandler<HTMLDetailsElement, Event> = (event) => setOpen(event.currentTarget.open);
-  const onLinkClick = () => {
-    setOpen(false);
-  };
+  const onLinkClick = () => setOpen(false);
   return (
     <nav
       id="table-of-contents"
@@ -28,7 +44,7 @@ export const TableOfContents: Component = () => {
           <span id="current-section"></span>
         </summary>
         <ul class="pl-2">
-          <For each={listItems}>
+          <For each={pageHeadings}>
             {(item) => (
               <li>
                 <a onClick={onLinkClick} class="inline-flex w-full py-2" href={`#${item.id}`}>
