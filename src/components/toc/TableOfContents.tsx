73a7c02c1f228fcb5f34cbb2c21c5a6a653bd32a
Nicholas Nassar <niccholaspage@gmail.com>
2023-02-10 22:48:42 -0500

    Work on tracking open state of details pane and closing it when clicking a link

--- src/components/toc/TableOfContents.tsx
+++ src/components/toc/TableOfContents.tsx
@@ -1,6 +1,6 @@
 import { ChevronRight } from "../icons/ChevronRight";
 import { RoundedButtonDiv } from "../button/RoundedButton";
-import { For, type Component } from "solid-js";
+import { createSignal, For, type JSX, type Component } from "solid-js";
 import { getPageHeadings, PageHeading } from "../../util/page-headings";
 
 // TODO: Don't hardcode this
@@ -9,12 +9,17 @@ const pageHeadings = getPageHeadings("/");
 const listItems = [...pageHeadings].map((item) => JSON.parse(item)) as PageHeading[];
 
 export const TableOfContents: Component = () => {
+  const [open, setOpen] = createSignal(false);
+  const onToggle: JSX.EventHandler<HTMLDetailsElement, Event> = (event) => setOpen(event.currentTarget.open);
+  const onLinkClick = () => {
+    setOpen(false);
+  };
   return (
     <nav
       id="table-of-contents"
       class="bg-zinc-200 dark:bg-zinc-900 fixed top-16 w-full -mx-4 px-4 border-b-2 border-b-zinc-100 dark:border-b-zinc-800"
     >
-      <details class="w-full py-2 flex items-center">
+      <details open={open()} ontoggle={onToggle} class="w-full py-2 flex items-center">
         <summary class="flex items-center gap-4 marker:hidden list-none">
           <RoundedButtonDiv size={"sm"}>
             On this page
@@ -22,11 +27,11 @@ export const TableOfContents: Component = () => {
           </RoundedButtonDiv>
           <span id="current-section"></span>
         </summary>
-        <ul>
+        <ul class="pl-2">
           <For each={listItems}>
             {(item) => (
               <li>
-                <a class="inline-flex w-full py-2" href={`#${item.id}`}>
+                <a onClick={onLinkClick} class="inline-flex w-full py-2" href={`#${item.id}`}>
                   {item.title}
                 </a>
               </li>
