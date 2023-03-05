884497afc03d34768c059d484653a396ea311b9f
Nicholas Nassar <niccholaspage@gmail.com>
2023-03-04 22:09:23 -0500

    Initial testing with Sandpack

---
+++ src/components/sandpack/Sandpack.tsx
@@ -0,0 +1,35 @@
+import { ClientOptions, loadSandpackClient, SandboxSetup } from "@codesandbox/sandpack-client";
+import { Component, onCleanup, onMount } from "solid-js";
+
+export const Sandpack: Component = () => {
+  let iframeRef: HTMLIFrameElement;
+  onMount(async () => {
+    const content: SandboxSetup = {
+      files: {
+        "/package.json": {
+          code: JSON.stringify({
+            main: "index.js",
+            dependencies: {},
+          }),
+        },
+        "/index.js": {
+          code: `console.log("This is an example...");`,
+        },
+      },
+    };
+
+    const options: ClientOptions = {};
+
+    const client = await loadSandpackClient(iframeRef, content, options);
+
+    onCleanup(() => {
+      client.destroy();
+    });
+  });
+  return (
+    <div>
+      <p>I'm a sandpack!</p>
+      <iframe ref={iframeRef!} />
+    </div>
+  );
+};
